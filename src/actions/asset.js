b269cbf908e3a9ad3db647d7a52e28188e6f407b
jonassnellinckx <jo.snel@hotmail.com>
2018-01-12 13:36:13 +0100

    Update version to latest

--- src/actions/asset.js
+++ src/actions/asset.js
@@ -1,13 +1,13 @@
-import * as bdb from '../bdb' // eslint-disable-line import/no-namespace
+import * as bdb from '../bdb'; // eslint-disable-line import/no-namespace
 
 
 export default class Asset {
     constructor(appId, type, schema) {
-        this.appId = appId
-        this.assetType = type
-        this.schema = schema
-        this.assetCollection = `${this.assetType}s`
-        this.dataType = `${this.appId}:${this.assetType}`
+        this.appId = appId;
+        this.assetType = type;
+        this.schema = schema;
+        this.assetCollection = `${this.assetType}s`;
+        this.dataType = `${this.appId}:${this.assetType}`;
     }
 
     load(dispatch, getState) {
@@ -19,16 +19,16 @@ export default class Asset {
                             this.updateStore(transaction.id, dispatch, getState)
                         )
                 ))
-            )
+            );
     }
 
     create(metadataPayload, dispatch, getState) {
-        const { publicKey, privateKey } = getState().identity.keypair
-        const assetPayload = { type: this.dataType }
+        const { publicKey, privateKey } = getState().identity.keypair;
+        const assetPayload = { type: this.dataType };
         assetPayload[this.assetType] = {
             type: this.dataType,
             schema: this.schema
-        }
+        };
         return bdb.publish(
             publicKey,
             privateKey,
@@ -36,12 +36,12 @@ export default class Asset {
             metadataPayload
         )
             .then(tx => this.updateStore(tx.id, dispatch, getState))
-            .catch(err => console.error(err))
+            .catch(err => console.error(err));
     }
 
     transfer(transaction, toPublicKey, payload, dispatch, getState) {
-        const state = getState()
-        const { publicKey, privateKey } = state.identity.keypair
+        const state = getState();
+        const { publicKey, privateKey } = state.identity.keypair;
 
         return bdb.transfer(
             transaction,
@@ -50,37 +50,37 @@ export default class Asset {
             toPublicKey,
             payload
         )
-        .then(() => {
-            const assetId = bdb.getAssetId(transaction)
-            this.updateStore(assetId, dispatch, getState)
-        })
-        .catch((err) => console.error(err))
+            .then(() => {
+                const assetId = bdb.getAssetId(transaction);
+                this.updateStore(assetId, dispatch, getState);
+            })
+            .catch((err) => console.error(err));
     }
 
     updateStore(assetId, dispatch, getState) { // eslint-disable-line
         return bdb.listTransactions(assetId)
             .then((txList) => Promise.all(txList.map(tx => bdb.getTransaction(tx.id))))
             .then((txList) => {
-                const assetTransaction = JSON.parse(localStorage.getItem(assetId))
+                const assetTransaction = JSON.parse(localStorage.getItem(assetId));
 
                 if (assetTransaction.asset.data.type !== this.dataType) {
-                    return new Promise()
+                    return new Promise();
                 }
 
-                const assetFromState = getState()[this.assetCollection][assetId]
+                const assetFromState = getState()[this.assetCollection][assetId];
 
                 if (assetFromState) {
-                    return this._dispatch('UPDATE', txList, dispatch, getState)
+                    return this._dispatch('UPDATE', txList, dispatch, getState);
                 } else {
-                    return this._dispatch('ADD', txList, dispatch, getState)
+                    return this._dispatch('ADD', txList, dispatch, getState);
                 }
-            })
+            });
     }
 
     _dispatch(action, transactions, dispatch, getState) {
-        const state = getState()
-        const asset = transactions[0]
-        const unspent = transactions[transactions.length - 1]
+        const state = getState();
+        const asset = transactions[0];
+        const unspent = transactions[transactions.length - 1];
         const provenance = transactions.map((tx) => (
             {
                 metadata: tx.metadata,
@@ -89,10 +89,10 @@ export default class Asset {
                 to: tx.outputs,
                 _txId: tx.id
             }
-        ))
+        ));
 
-        const actionType = `${action}_${this.assetType.toUpperCase()}`
-        const dispatchObject = { type: actionType }
+        const actionType = `${action}_${this.assetType.toUpperCase()}`;
+        const dispatchObject = { type: actionType };
 
         switch (action) {
             case 'ADD':
@@ -103,18 +103,18 @@ export default class Asset {
                     _txId: unspent.id,
                     _assetID: asset.id,
                     provenance
-                }
-                return dispatch(dispatchObject)
+                };
+                return dispatch(dispatchObject);
             case 'UPDATE':
                 dispatchObject[this.assetType] = {
                     ...state[this.assetCollection][bdb.getAssetId(unspent)],
                     metaData: unspent.metadata,
                     _txId: unspent.id,
                     provenance
-                }
-                return dispatch(dispatchObject)
+                };
+                return dispatch(dispatchObject);
             default:
-                return null
+                return null;
         }
     }
 }
--- src/bdb.js
+++ src/bdb.js
@@ -1,18 +1,18 @@
-import reconnectCore from 'reconnect-core'
-import SimpleWebsocket from 'simple-websocket'
-import * as driver from 'bigchaindb-driver' // eslint-disable-line import/no-namespace
+import reconnectCore from 'reconnect-core';
+import SimpleWebsocket from 'simple-websocket';
+import * as driver from 'bigchaindb-driver'; // eslint-disable-line import/no-namespace
 
 
-export const BDB_SERVER_URL = process.env.REACT_APP_BDB_SERVER_URL || 'http://localhost:49984'
-export const BDB_API_PATH = `${BDB_SERVER_URL}/api/v1/`
-export const BDB_WS_URL = process.env.REACT_APP_BDB_WS_URL || 'http://localhost:49985'
-export const BDB_WS_PATH = `${BDB_WS_URL}/api/v1/streams/valid_transactions`
-console.log('BDB_SERVER_URL', BDB_SERVER_URL) // eslint-disable-line no-console
-console.log('BDB_WS_URL', BDB_WS_URL) // eslint-disable-line no-console
+export const BDB_SERVER_URL = process.env.REACT_APP_BDB_SERVER_URL || 'http://localhost:49984';
+export const BDB_API_PATH = `${BDB_SERVER_URL}/api/v1/`;
+export const BDB_WS_URL = process.env.REACT_APP_BDB_WS_URL || 'http://localhost:49985';
+export const BDB_WS_PATH = `${BDB_WS_URL}/api/v1/streams/valid_transactions`;
+console.log('BDB_SERVER_URL', BDB_SERVER_URL); // eslint-disable-line no-console
+console.log('BDB_WS_URL', BDB_WS_URL); // eslint-disable-line no-console
 
-const conn = new driver.Connection(BDB_API_PATH)
+const conn = new driver.Connection(BDB_API_PATH);
 
-export const keypair = (seed) => new driver.Ed25519Keypair(seed.slice(0, 32))
+export const keypair = (seed) => new driver.Ed25519Keypair(seed.slice(0, 32));
 
 export const publish = (publicKey, privateKey, payload, metadata) => {
     // Create a transation
@@ -24,140 +24,139 @@ export const publish = (publicKey, privateKey, payload, metadata) => {
                 driver.Transaction.makeEd25519Condition(publicKey))
         ],
         publicKey
-    )
+    );
     // sign/fulfill the transaction
-    const txSigned = driver.Transaction.signTransaction(tx, privateKey)
+    const txSigned = driver.Transaction.signTransaction(tx, privateKey);
 
     // send it off to BigchainDB
     return conn.postTransaction(txSigned)
         .then(() => conn.pollStatusAndFetchTransaction(txSigned.id))
-        .then(() => txSigned)
-}
+        .then(() => txSigned);
+};
 
 export const getUnspents = (publicKey, spent = undefined, callback) => // eslint-disable-line no-unused-vars
     conn.listOutputs(publicKey, spent)
-        .then(unspents => unspents.map(elem => elem.transaction_id))
+        .then(unspents => unspents.map(elem => elem.transaction_id));
 
 export const getTransaction = (txId) =>
     new Promise((resolve, reject) => {
-        const txLocal = localStorage.getItem(txId)
+        const txLocal = localStorage.getItem(txId);
         if (txLocal) {
-            resolve(JSON.parse(txLocal))
+            resolve(JSON.parse(txLocal));
         } else {
-            let txFetched
-            let blockFetched
+            let txFetched;
+            let blockFetched;
             conn.getTransaction(txId)
                 .then(tx => {
-                    txFetched = tx
-                    return conn.listBlocks(txId)
+                    txFetched = tx;
+                    return conn.listBlocks(txId);
                 })
                 .then(block => {
-                    blockFetched = block
-                    return conn.listVotes(block[0])
+                    blockFetched = block;
+                    return conn.listVotes(block[0]);
                 })
                 .then(votes => {
                     txFetched = {
                         ...txFetched,
                         block: blockFetched,
                         votes
-                    }
-                    localStorage.setItem(txId, JSON.stringify(txFetched))
-                    resolve(txFetched)
+                    };
+                    localStorage.setItem(txId, JSON.stringify(txFetched));
+                    resolve(txFetched);
                 })
-                .catch(reason => reject(reason))
+                .catch(reason => reject(reason));
         }
-    })
+    });
 
 export const listTransactions = (assetId) =>
     conn.listTransactions(assetId)
         .then((txList) => {
             if (txList.length <= 1) {
-                return txList
+                return txList;
             }
-            const inputTransactions = []
+            const inputTransactions = [];
             txList.forEach((tx) =>
                 tx.inputs.forEach(input => {
                     if (input.fulfills) {
-                        inputTransactions.push(input.fulfills.transaction_id)
+                        inputTransactions.push(input.fulfills.transaction_id);
                     }
                 })
-            )
-            const unspents = txList.filter((tx) => inputTransactions.indexOf(tx.id) === -1)
+            );
+            const unspents = txList.filter((tx) => inputTransactions.indexOf(tx.id) === -1);
             if (unspents.length) {
-                let tipTransaction = unspents[0]
-                let tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id
-                const sortedTxList = []
+                let tipTransaction = unspents[0];
+                let tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id;
+                const sortedTxList = [];
                 while (true) { // eslint-disable-line no-constant-condition
-                    sortedTxList.push(tipTransaction)
+                    sortedTxList.push(tipTransaction);
                     try {
-                        tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id
+                        tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id;
                     } catch (e) {
-                        break
+                        break;
                     }
                     if (!tipTransactionId) {
-                        break
+                        break;
                     }
                     tipTransaction = txList.filter((tx) => // eslint-disable-line no-loop-func
-                        tx.id === tipTransactionId)[0]
+                        tx.id === tipTransactionId)[0];
                 }
-                return sortedTxList.reverse()
+                return sortedTxList.reverse();
             } else {
                 console.error('something went wrong while sorting transactions',
-                    txList, inputTransactions)
+                    txList, inputTransactions);
             }
-            return txList
-        })
+            return txList;
+        });
 
 export const searchAssets = (search) =>
     conn.searchAssets(search)
-        .then(assetList => assetList.map(asset => asset.id))
+        .then(assetList => assetList.map(asset => asset.id));
 
 export const transfer = (tx, fromPublicKey, fromPrivateKey, toPublicKey, metadata) => {
     const txTransfer = driver.Transaction.makeTransferTransaction(
-        tx,
-        metadata,
+        [{ tx, output_index: 0 }],
         [
             driver.Transaction.makeOutput(
                 driver.Transaction.makeEd25519Condition(toPublicKey)
             )
         ],
-        0
-    )
+        metadata
+    );
 
-    const txTransferSigned = driver.Transaction.signTransaction(txTransfer, fromPrivateKey)
+    const txTransferSigned = driver.Transaction.signTransaction(txTransfer, fromPrivateKey);
     // send it off to BigchainDB
     return conn.postTransaction(txTransferSigned)
         .then(() =>
             conn.pollStatusAndFetchTransaction(txTransferSigned.id))
-        .then(() => txTransferSigned)
-}
+        .then(() => txTransferSigned);
+};
 
 export function connect(handleEvent) {
-    console.log(`subscribing to ${BDB_WS_PATH}`) // eslint-disable-line no-console
+    console.log(`subscribing to ${BDB_WS_PATH}`); // eslint-disable-line no-console
 
-    const reconnect = reconnectCore(() => new SimpleWebsocket(BDB_WS_PATH))
+    const reconnect = reconnectCore(() => new SimpleWebsocket(BDB_WS_PATH));
 
     return new Promise((resolve, reject) => {
         this.connection = reconnect({ immediate: true }, (ws) => {
             ws.on('open', () => {
-                console.log(`ws connected to ${BDB_WS_PATH}`) // eslint-disable-line no-console
-            })
+                console.log(`ws connected to ${BDB_WS_PATH}`); // eslint-disable-line no-console
+            });
             ws.on('data', (msg) => {
-                const ev = JSON.parse(msg)
-                handleEvent(ev)
-            })
+                const ev = JSON.parse(msg);
+                handleEvent(ev);
+            });
             ws.on('close', () => {
-                console.log(`ws disconnected from ${BDB_WS_PATH}`) // eslint-disable-line no-console
-            })
+                console.log(`ws disconnected from ${BDB_WS_PATH}`); // eslint-disable-line no-console
+            });
         })
-        .once('connect', () => resolve(null))
+            .once('connect', () => resolve(null))
             .on('error', (err) => {
-                console.warn(`ws error on ${BDB_WS_PATH}:  ${err}`) // eslint-disable-line no-console
-                reject(err)
+                console.warn(`ws error on ${BDB_WS_PATH}:  ${err}`); // eslint-disable-line no-console
+                reject(err);
             })
-            .connect()
-    })
+            .connect();
+    });
 }
 
 export const getAssetId = (tx) => // eslint-disable-line
-    tx.operation === 'CREATE' ? tx.id : tx.asset.id
+    tx.operation === 'CREATE' ? tx.id : tx.asset.id;
